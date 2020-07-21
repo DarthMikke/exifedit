410c13b547159284b7212380ef79435892ef027d
Michal Jan Warecki <mwarecki1@gmail.com>
2020-07-21 15:34:18 +0200

    Initial Commit

---
+++ EXIF viewer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  EXIF viewer
+//
+//  Created by Michal Jan Warecki on 21/07/2020.
+//  Copyright © 2020 Michal Jan Warecki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
