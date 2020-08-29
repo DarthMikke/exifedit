f2a6643e1b79cb6df6a40bf55a63fcabc384e62a
Michal Jan Warecki <mwarecki1@gmail.com>
2020-08-29 10:49:04 +0200

    Funksjon runCommand

--- EXIF viewer/functions.swift
+++ EXIF viewer/functions.swift
@@ -110,3 +110,37 @@ func unwrapProperty(property: String, newValue: String, withData:Dictionary<Stri
     
     return unwrapped
 }
+
+func runCommand(cmd : String, args : String...) -> (output: [String], error: [String], exitCode: Int32) {
+
+    var output : [String] = []
+    var error : [String] = []
+
+    let task = Process()
+    task.executableURL = URL(fileURLWithPath: cmd)
+    task.arguments = args
+
+    let outpipe = Pipe()
+    task.standardOutput = outpipe
+    let errpipe = Pipe()
+    task.standardError = errpipe
+    
+    try! task.run()
+
+    let outdata = outpipe.fileHandleForReading.readDataToEndOfFile()
+    if var string = String(data: outdata, encoding: .utf8) {
+        string = string.trimmingCharacters(in: .newlines)
+        output = string.components(separatedBy: "\n")
+    }
+
+    let errdata = errpipe.fileHandleForReading.readDataToEndOfFile()
+    if var string = String(data: errdata, encoding: .utf8) {
+        string = string.trimmingCharacters(in: .newlines)
+        error = string.components(separatedBy: "\n")
+    }
+
+    task.waitUntilExit()
+    let status = task.terminationStatus
+
+    return (output, error, status)
+}
