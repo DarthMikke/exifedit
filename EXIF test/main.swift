5892e7f4eb8caec4a2b002e0763ec38134555299
Michal Jan Warecki <mwarecki1@gmail.com>
2020-11-07 14:57:46 +0100

    Entitlements og ny test

---
+++ EXIF test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EXIF test
+//
+//  Created by Michal Jan Warecki on 20/08/2020.
+//  Copyright © 2020 Michal Jan Warecki. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
